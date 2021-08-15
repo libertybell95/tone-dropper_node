ef08029f0c336cae00553d228fdce0a255addd88
Joshua Bell <39743780+libertybell95@users.noreply.github.com>
2021-08-15 14:19:53 -0500

    debugging

--- client/src/components/Tone.jsx
+++ client/src/components/Tone.jsx
@@ -6,36 +6,39 @@ import {
 
 const Tone = ({ tone, onToneChange, duration, onDurationChange, onDelete, availableTones, id }) => {
   return (
-    <tr>
-      <td style={{ marginLeft: '.3em', marginRight: '.3em', width: '10em' }}>
-        <select
-          value={tone}
-          onChange={onToneChange}
-          className='form-select'
-          style={{ float: 'left' }}
-        >
-          <option value='' />
-          {availableTones.map((val, idx) => (
-            <option key={idx} value={val[0]}>{val[0]}</option>
-          ))}
-        </select>
-      </td>
-      <td style={{ marginLeft: '.3em', marginRight: '.3em', width: '10em' }}>
-        <FormControl
-          type='number'
-          value={duration}
-          onChange={onDurationChange}
-        />
-      </td>
-      <td style={{ marginLeft: '.3em', marginRight: '.3em', width: '5em' }}>
-        <Button
-          variant='danger'
-          onClick={() => onDelete(id)}
-        >
-          Delete
-        </Button>
-      </td>
-    </tr>
+    <tbody>
+
+      <tr>
+        <td style={{ marginLeft: '.3em', marginRight: '.3em', width: '10em' }}>
+          <select
+            value={tone}
+            onChange={onToneChange}
+            className='form-select'
+            style={{ float: 'left' }}
+          >
+            <option value='' />
+            {availableTones.map((val, idx) => (
+              <option key={idx} value={val[0]}>{val[0]}</option>
+            ))}
+          </select>
+        </td>
+        <td style={{ marginLeft: '.3em', marginRight: '.3em', width: '10em' }}>
+          <FormControl
+            type='number'
+            value={duration}
+            onChange={onDurationChange}
+          />
+        </td>
+        <td style={{ marginLeft: '.3em', marginRight: '.3em', width: '5em' }}>
+          <Button
+            variant='danger'
+            onClick={() => onDelete(id)}
+          >
+            Delete
+          </Button>
+        </td>
+      </tr>
+    </tbody>
   )
 }
 
